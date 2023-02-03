b73e0e4d9dbc4ab364b919d93c93b60f862717a2
Kyle-Helmer <99497831+Kyle-Helmer@users.noreply.github.com>
2023-02-03 13:41:34 -0700

    Added US methods to LocationService and LocationDB classes

--- src/java/dataaccess/LocationDB.java
+++ src/java/dataaccess/LocationDB.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 import models.Canadalocation;
+import models.Uslocation;
 
 /**
  *
@@ -21,6 +22,8 @@ public class LocationDB {
 
     }
 
+       //Canada related methods 
+    
     public List<Canadalocation> getAllCan() throws Exception {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
 
@@ -100,6 +103,94 @@ public class LocationDB {
         try {
             trans.begin();
             em.remove(em.merge(canLoc));
+            trans.commit();
+        } finally {
+            em.close();
+        }
+
+    }
+    
+       //Us related methods 
+    
+    public List<Uslocation> getAllUs() throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            List<Uslocation> usLocations = em.createNamedQuery("Uslocation.findAll", Uslocation.class).getResultList();
+            return usLocations;
+
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public Uslocation getUs(String zipCode) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            Uslocation us = em.find(Uslocation.class, zipCode);
+            return us;
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Uslocation> getAllUsState(String state) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            Query queryUsLocByregion = em.createNamedQuery("Uslocation.findByState");
+            queryUsLocByregion.setParameter("state", state);
+            List<Uslocation> usLocations = queryUsLocByregion.getResultList();
+            return usLocations;
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void insertUs(Uslocation usLoc) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+
+            trans.begin();
+            em.persist(usLoc);
+            trans.commit();
+
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void updateUs(Uslocation usLoc) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.persist(usLoc);
+            trans.commit();
+        } finally {
+            em.close();
+        }
+
+    }
+
+    public void deleteUs(Uslocation usLoc) {
+
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.remove(em.merge(usLoc));
             trans.commit();
         } finally {
             em.close();
--- src/java/services/LocationService.java
+++ src/java/services/LocationService.java
@@ -7,6 +7,7 @@ package services;
 import dataaccess.LocationDB;
 import models.Canadalocation;
 import java.util.List;
+import models.Uslocation;
 /**
  *
  * @author Kyle Helmer
@@ -15,6 +16,8 @@ import java.util.List;
 public class LocationService {
     
     private LocationDB locDB = new LocationDB();
+    
+       //Canada related methods 
     
     public List<Canadalocation> getAllCan() throws Exception{
         
@@ -44,6 +47,39 @@ public class LocationService {
     public void deleteCan(String locationCode){
         Canadalocation canLoc = locDB.getCan(locationCode);
         locDB.deleteCan(canLoc);
+    }
+    
+    //Us related methods 
+    
+    
+    public List<Uslocation> getAllUs() throws Exception{
+        
+        return locDB.getAllUs();
+    }
+    
+    public Uslocation getUs(String zipCode){
+       return locDB.getUs(zipCode);
+    }
+    
+     public List<Uslocation> getAllUsRegion(String state) throws Exception{
+        return locDB.getAllUsState(state);
+    }
+    
+    public void insertUs(String zipCode, String country, String state){
+        Uslocation usLoc = new Uslocation(zipCode, country, state);
+        locDB.insertUs(usLoc);
+    }
+    
+    public void updateUs(String zipCode, String country, String state){
+       Uslocation usLoc = locDB.getUs(zipCode);
+        usLoc.setCountry(country);
+        usLoc.setState(state);
+        locDB.updateUs(usLoc);   
+    }
+    
+    public void deleteUs(String zipCode){
+        Uslocation usLoc = locDB.getUs(zipCode);
+        locDB.deleteUs(usLoc);
     }
     
     

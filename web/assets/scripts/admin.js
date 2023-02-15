ff4eecbaa4aec9cb200d5e1efe827ac6489951aa
Leo Talbot <leomtalbot@gmail.com>
2023-02-15 13:19:47 -0700

    Updated javascript for admin page hide and show functionality completed

--- web/assets/scripts/admin.js
+++ web/assets/scripts/admin.js
@@ -1,10 +1,43 @@
-function revealAdd() {
-  document.getElementById("add").classList.remove("hidden");
-  document.getElementById("search").classList.add("hidden");
-  document.getElementById("searchTable").classList.add("hidden");
+function hideForm(formID) {
+    document.getElementById(formID).classList.add("hidden");
 }
-function revealSearch() {
-  document.getElementById("search").classList.remove("hidden");
-  document.getElementById("searchTable").classList.remove("hidden");
-  document.getElementById("add").classList.add("hidden");
+
+function showForm(formID) {
+    document.getElementById(formID).classList.remove("hidden");
+}
+
+function showSearchForm() {
+    showForm("searchForm");
+    showForm("searchTable");
+    
+    hideForm("addForm");
+    hideForm("editForm");
+    hideForm("deleteForm");
+}
+
+function showAddForm() {
+    showForm("addForm");
+    
+    hideForm("searchForm");
+    hideForm("searchTable");
+    hideForm("editForm");
+    hideForm("deleteForm");
+}
+
+function showEditForm() {
+    showForm("editForm");
+    
+    hideForm("searchForm");
+    hideForm("searchTable");
+    hideForm("addForm");
+    hideForm("deleteForm");
+}
+
+function showDeleteForm() {
+    showForm("deleteForm");
+    
+    hideForm("searchForm");
+    hideForm("searchTable");
+    hideForm("addForm");
+    hideForm("editForm");
 }
